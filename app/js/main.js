d943be5c1816425994f6d1de0272c71d6f312031
Olesya <olesyabrazhnikova@ukr.net>
2023-04-19 16:00:04 +0300

    added files

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+document.querySelector('.item').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker2').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker3').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker4').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker5').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker6').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker7').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
+document.querySelector('#sticker8').addEventListener('click', function(){
+    document.getElementById('elem-swipe').classList.add("hide-swipe");
+    this.classList.add('hide')
+})
